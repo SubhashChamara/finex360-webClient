b5673dd28693ecfa3cc51294678ecde9f84cca98
Subhash Chamara <udayanga.subhash94@gmail.com>
2025-09-21 16:56:59 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
+import 'dotenv/config';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const apiBase = process.env.NEXT_PUBLIC_API_BASE_URL;
+    if (!apiBase) {
+      throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined!");
+    }
+
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+        destination: `${apiBase}/api/:path*`,
       },
     ];
   },
