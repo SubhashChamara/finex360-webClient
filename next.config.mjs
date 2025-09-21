31ea1292d2e40600b782e38849b7591f5e6192d4
Subhash Chamara <udayanga.subhash94@gmail.com>
2025-09-21 15:08:14 +0530

    api path set2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+import dotenv from 'dotenv'; // Import dotenv
+
+// Load environment variables based on the current environment
+// This ensures NEXT_PUBLIC_API_BASE_URL is available during build time
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: '.env.local' });
+} else if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' });
+} else { // Assuming UAT or other custom environments
+  dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+}
+
+
 const nextConfig = {
   async rewrites() {
     return [
